d83d606d4819dcc3c2356d7f2c411368745c9825
Jaskirat Singh <101934580+JaskiratSingh08@users.noreply.github.com>
2022-11-30 17:27:44 +0530

    further improvements

--- static/js/script.js
+++ static/js/script.js
@@ -90,3 +90,21 @@ function predictResult(fileURL) {
     console.error(e);
   }
 }
+
+
+// for pop-up
+
+
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      500 
+  )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
